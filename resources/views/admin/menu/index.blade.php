23b52b3ae539ac969e846bc2fd76899851089cb9
egiska33 <esivickas@gmail.com>
2018-01-02 10:34:37 +0200

    first commit

---
+++ resources/views/admin/menu/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="col-md-8">
+        @if(session('message'))
+            <div class="alert alert-info">{{session('message')}}</div>
+        @endif
+        <ul class="list-group">
+            @foreach($menus as $menu)
+
+                <li class="list-group-item">
+                    {{ $menu->title }}
+                    <form action="{{ route('menu.destroy', $menu) }}" method="POST"
+                          style="display: inline"
+                          onsubmit="return confirm('Are you sure?');">
+                        <input type="hidden" name="_method" value="DELETE">
+                        {{ csrf_field() }}
+                        <button class="btn btn-danger pull-right">Delete</button>
+                    </form>
+                    <a href="{{route('menu.edit', $menu)}}" class="btn btn-primary pull-right">Edit</a>
+                </li>
+
+            @endforeach
+
+        </ul>
+
+
+    </div>
+    <div class="col-md-8">
+        <a href="{{route('admin')}}" class="btn btn-default pull-left">Back</a>
+        <a href="{{route('menu.create')}}" class="btn btn-primary pull-right">Create</a>
+
+    </div>
+
+@endsection
+
